d033be691fc1d56d292871da28c4aa9596044efc
André Mendes <andregmendes@gmail.com>
2023-11-24 01:23:22 +0000

    Build done @ VERCEL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-// TSX Components
+// IMPORTING NEXTJS COMPONENTS
 import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
