5ff36305ed20f98725c4b1cc39e8965de9083320
André Mendes <andregmendes@gmail.com>
2023-11-24 12:10:19 +0000

    Playing around with TAILWIND

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,19 +2,20 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full absolute text-white z-10">
+    <div className="w-full absolute text-white z-10 ">
       <nav className="container relative flex flex-wrap item-center justify-between mx-auto p-8">
         <Link href="/" className="font-bond text-3xl">
           Home
         </Link>
-        <div className="space-x-4 text-xl">
-          <Link href="/performance">Performance</Link>
-          <Link href="/reliability">Reliability</Link>
-          <Link href="/scale">Scale</Link>
+        <div className="space-x-4 text-xl flex items-end">
+          <Link href="/performance" className="text-2xl font-bold text-indigo-500/100 hover:text-emerald-400 ">Performance</Link>
+          <Link href="/reliability" className="text-2xl font-bold text-indigo-500/100 hover:text-emerald-400">Reliability</Link>
+          <Link href="/scale" className="text-2xl font-bold text-indigo-500/100 hover:text-emerald-400">Scale</Link>
         </div>
       </nav>
     </div>
   );
 };
+
 
 export default Header;
